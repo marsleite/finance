26a8a02eea18bf5e9c535f45be40bc5490b88ead
Marcelo <marcelo.leite@mercadolivre.com>
2023-07-05 08:40:34 -0300

    [feat] config adjust script

--- scripts/check_coverage.py
+++ scripts/check_coverage.py
@@ -1,30 +1,35 @@
 import os
 from bs4 import BeautifulSoup
 
-# Caminho do relatório de cobertura
-report_path = "./build/reports/jacoco/test/html/index.html"
+# Define o caminho para o arquivo index.html
+index_file_path = "./build/reports/jacoco/test/html/index.html"
 
-# Verifica se o arquivo de relatório existe
-if not os.path.isfile(report_path):
-    print("O arquivo de relatório não foi encontrado.")
+# Verifica se o arquivo index.html existe
+if not os.path.exists(index_file_path):
+    print("O arquivo index.html não foi encontrado.")
     exit(1)
 
-# Abre o arquivo de relatório
-with open(report_path, "r") as f:
-    content = f.read()
+# Lê o conteúdo do arquivo index.html
+with open(index_file_path, "r", encoding="utf-8") as file:
+    content = file.read()
 
-# Analisa o conteúdo HTML do relatório
+# Analisa o conteúdo HTML com BeautifulSoup
 soup = BeautifulSoup(content, "html.parser")
 
-# Extrai a porcentagem de linhas cobertas
-line_coverage_element = soup.find("th", text="Line, %")
-line_coverage = line_coverage_element.find_next_sibling("td").text.strip()
+# Encontra o elemento da tabela com a porcentagem de linhas cobertas
+line_coverage_element = soup.find("th", string="Line, %")
+if line_coverage_element is None:
+    print("Elemento 'Line, %' não encontrado no arquivo index.html.")
+    exit(1)
 
-# Converte a porcentagem em um número decimal
-line_coverage = float(line_coverage.rstrip("%")) / 100
+# Obtém a porcentagem de cobertura de linhas
+line_coverage = line_coverage_element.find_next_sibling("td").find("span", class_="percent").get_text(strip=True)
 
-# Verifica se a cobertura de linha é maior ou igual a 90%
-if line_coverage >= 0.9:
-    print("A cobertura de linha é satisfatória.")
+# Converte a porcentagem em um valor numérico
+line_coverage_percentage = float(line_coverage.strip("%"))
+
+# Verifica se a cobertura de linhas é maior ou igual a 90%
+if line_coverage_percentage >= 90:
+    print(f"A cobertura de linhas é de {line_coverage_percentage}%. Parabéns!")
 else:
-    print("A cobertura de linha é insuficiente.")
+    print(f"A cobertura de linhas é de {line_coverage_percentage}%. É necessário melhorar a cobertura.")
