7a401f04c1587df9893451639e1a8e557e667e79
Marcelo <marcelo.leite@mercadolivre.com>
2023-07-05 08:10:30 -0300

    [feat] config adjust script

--- scripts/check_coverage.py
+++ scripts/check_coverage.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 
 def check_coverage():
-    tree = ET.parse('./build/reports/jacoco/test/jacocoTestReport.xml')
+    tree = ET.parse('./build/reports/jacoco/test/jacocoTestReport.xml')  # Caminho para o relatório XML gerado pelo Kover
     root = tree.getroot()
 
     total_lines = 0
@@ -12,8 +12,11 @@ def check_coverage():
             for sourcefile in clazz.findall('sourcefile'):
                 for line in sourcefile.findall('line'):
                     total_lines += 1
-                    if line.attrib['ci'] != '0':
-                        covered_lines += 1
+                    covered_lines += int(line.attrib.get('ci', 0))
+
+    if total_lines == 0:
+        print('No lines found in the coverage report.')
+        return
 
     coverage_percentage = (covered_lines / total_lines) * 100
 
