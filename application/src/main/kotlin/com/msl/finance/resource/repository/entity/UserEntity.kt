1db8a7a29703eb3bba3aba22382f9499f6ab3bf7
Marcelo <marcelo.leite@mercadolivre.com>
2023-07-07 16:25:06 -0300

    [feat] created RepositoryException

--- application/src/main/kotlin/com/msl/finance/resource/repository/entity/UserEntity.kt
+++ application/src/main/kotlin/com/msl/finance/resource/repository/entity/UserEntity.kt
@@ -1,13 +1,14 @@
 package com.msl.finance.resource.repository.entity
 
+import com.msl.finance.model.User
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
 
-@Table(name = "user")
+@Table(name = "users")
 data class UserEntity(
     @Id
-    val id: String,
+    val id: String?,
     val firstName: String,
     val lastName: String,
     val email: String,
@@ -15,4 +16,27 @@ data class UserEntity(
     val password: String,
     val createdAt: LocalDateTime,
     val updatedAt: LocalDateTime?
-)
+) {
+
+    constructor(user: User): this(
+        id = null,
+        firstName = user.firstName,
+        lastName = user.lastName,
+        email = user.email,
+        gender = user.gender,
+        password = user.password,
+        createdAt = LocalDateTime.now(),
+        updatedAt = user.createdAt
+    )
+
+    fun toDomain() = User(
+        id = checkNotNull(id),
+        firstName = firstName,
+        lastName = lastName,
+        email = email,
+        gender = gender,
+        password = password,
+        createdAt = createdAt,
+        updatedAt = updatedAt
+    )
+}
--- application/src/main/kotlin/com/msl/finance/resource/repository/sql/Impl/UserRepositoryImpl.kt
+++ application/src/main/kotlin/com/msl/finance/resource/repository/sql/Impl/UserRepositoryImpl.kt
@@ -2,9 +2,16 @@ package com.msl.finance.resource.repository.sql.Impl
 
 import com.msl.finance.gateway.UserRepository
 import com.msl.finance.model.User
+import com.msl.finance.resource.repository.entity.UserEntity
+import com.msl.finance.resource.repository.sql.spring.UserRepositorySpring
+import org.springframework.stereotype.Component
 
-class UserRepositoryImpl: UserRepository {
-    override suspend fun registerUser(user: User): User {
-        TODO("Not yet implemented")
-    }
+@Component
+class UserRepositoryImpl(
+  private val userRepositorySpring: UserRepositorySpring
+): UserRepository {
+  override suspend fun registerUser(user: User): User {
+    return userRepositorySpring.save(UserEntity(user)).toDomain()
+  }
+
 }
